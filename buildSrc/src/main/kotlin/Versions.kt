6ed051f506b9612a426c7a4eb346d63e6864e427
steve <steve@androidbox.me>
2019-05-20 22:17:22 +0700

    Optimized build times

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -7,8 +7,8 @@ object Versions {
     const val compileSdkVersion = 28
     const val versionCode = 1
     const val versionName = "1.0"
-    const val kotlinVersion = "1.3.0"
-    const val gradleVersion = "3.3.2"
+    const val kotlinVersion = "1.3.31"
+    const val gradleVersion = "3.4.1"
     const val androidXVersion = "1.0.2"
     const val gsonVersion = "2.8.5"
     const val retrofitVersion = "2.5.0"
