e3fe87165d52a10f787cd4186497033c3bd098db
steve mason <stevekitkat@gmail.com>
2019-11-13 23:26:02 +0700

    Adds more data to the entity classes in the data layer

--- data/src/main/java/androidbox/me/entities/ForecastEntity.kt
+++ data/src/main/java/androidbox/me/entities/ForecastEntity.kt
@@ -1,8 +1,12 @@
 package androidbox.me.entities
 
+import androidbox.me.entities.request.WeatherEntity
 import com.google.gson.annotations.SerializedName
 
 data class ForecastEntity(
+    @SerializedName("temp") val temp: Float,
     @SerializedName("high_temp") val highTemp: Float,
-    @SerializedName("app_min_temp") val minTemp: Float,
-    @SerializedName("app_max_temp") val maxTemp: Float)
+    @SerializedName("low_temp") val lowTemp: Float,
+    @SerializedName("app_min_temp") val feelsLikeMinTemp: Float,
+    @SerializedName("app_max_temp") val feelsLikeMaxTemp: Float,
+    @SerializedName("weather") val weather: WeatherEntity)
---
+++ data/src/main/java/androidbox/me/entities/request/WeatherEntity.kt
@@ -0,0 +1,9 @@
+package androidbox.me.entities.request
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherEntity(
+    @SerializedName("icon") val icon: String,
+    @SerializedName("code") val code: Int,
+    @SerializedName("description") val description: String
+)
