28a5f497ebfaaa575c828ba42457d40537e44e7b
steve mason <stevekitkat@gmail.com>
2019-12-07 16:08:44 +0700

    Adding livedata

--- presentation/src/main/java/me/androidbox/presentation/forecast/mvvm/ForecastViewModel.kt
+++ presentation/src/main/java/me/androidbox/presentation/forecast/mvvm/ForecastViewModel.kt
@@ -3,6 +3,7 @@ package me.androidbox.presentation.forecast.mvvm
 import androidx.lifecycle.MutableLiveData
 import io.reactivex.disposables.CompositeDisposable
 import me.androidbox.presentation.base.BaseViewModel
+import me.androidbox.presentation.models.WeatherForecast
 import me.androidbox.presentation.utils.NetworkHelper
 
 class ForecastViewModel(
@@ -10,9 +11,9 @@ class ForecastViewModel(
     networkHelper: NetworkHelper)
     : BaseViewModel(compositeDisposable, networkHelper) {
 
-    val data = MutableLiveData<String>()
+    val data = MutableLiveData<WeatherForecast>()
 
     override fun onCreate() {
-        data.postValue("ForecastViewModel")
+        // data.postValue()
     }
 }
