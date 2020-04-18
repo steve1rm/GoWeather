dbaec6630018ba1302d06f7041162127450b666f
steve mason <stevekitkat@gmail.com>
2020-04-19 01:18:18 +0700

    Adds screenshot tests

---
+++ presentation/src/androidTest/java/me/androidbox/presentation/forecast/screentests/ForecastLoadingTest.kt
@@ -0,0 +1,53 @@
+package me.androidbox.presentation.forecast.screentests
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.facebook.testing.screenshot.Screenshot
+import com.facebook.testing.screenshot.ViewHelpers
+import me.androidbox.presentation.R
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ForecastLoadingTest {
+
+    @Test
+    fun shouldShowLoadingScreen() {
+        // Arrange
+        val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
+        val loadingView: View = LayoutInflater.from(context).inflate(R.layout.inprogress, null, false)
+
+        ViewHelpers
+            .setupView(loadingView)
+            .setExactWidthDp(360)
+            .layout()
+
+        // Snap
+        Screenshot
+            .snap(loadingView)
+            .record()
+    }
+
+
+    @Test
+    fun shouldShowRetryScreen() {
+        // Arrange
+        val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
+        val retryView: View = LayoutInflater.from(context).inflate(R.layout.failurecase_layout, null, false)
+
+        ViewHelpers
+            .setupView(retryView)
+            .guessListViewHeight()
+            .setExactWidthDp(360)
+            .layout()
+
+        // Snap
+        Screenshot
+            .snap(retryView)
+            .record()
+
+    }
+}
