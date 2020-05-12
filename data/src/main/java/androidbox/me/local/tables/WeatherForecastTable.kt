d9b935556f8cc5974b4656d8f46b5883c8c36038
Steve <stevekitkat@gmail.com>
2020-05-12 18:07:44 +0700

    Completed WeatherForecastTable relationship

--- data/src/main/java/androidbox/me/local/tables/WeatherForecastTable.kt
+++ data/src/main/java/androidbox/me/local/tables/WeatherForecastTable.kt
@@ -2,9 +2,15 @@ package androidbox.me.local.tables
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "weatherForecastTable")
+@Entity(tableName = "weatherForecastTable",
+    foreignKeys = [ForeignKey(
+        entity = ForecastTable::class,
+        parentColumns = ["id"],
+        childColumns = ["forecastId"],
+        onDelete = ForeignKey.CASCADE)])
 data class WeatherForecastTable(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
@@ -19,4 +25,7 @@ data class WeatherForecastTable(
     val countryCode: String,
 
     @ColumnInfo(name = "stateCode")
-    val stateCode: String)
+    val stateCode: String,
+
+    @ColumnInfo(name = "forecastId")
+    val forecastId: Long)
