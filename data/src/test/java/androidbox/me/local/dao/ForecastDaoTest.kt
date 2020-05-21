0633f9a45f08142d7d1210a07839f9a45e20b401
Steve <stevekitkat@gmail.com>
2020-05-21 17:43:05 +0700

    Completed test coverage for ForecastDao

--- data/src/test/java/androidbox/me/local/dao/ForecastDaoTest.kt
+++ data/src/test/java/androidbox/me/local/dao/ForecastDaoTest.kt
@@ -109,4 +109,91 @@ class ForecastDaoTest {
             .assertComplete()
             .assertValue(forecastTableList)
     }
+
+    @Test
+    fun `should get forecast with specific ID`() {
+        // Arrange
+        val weatherTableOne = WeatherFactory.createWeather()
+        val weatherTableTwo = WeatherFactory.createWeather()
+        val weatherTableThree = WeatherFactory.createWeather()
+
+        val forecastTableOne = ForecastFactory.createForecast().copy(weatherId = 1)
+        val forecastTableTwo = ForecastFactory.createForecast().copy(weatherId = 2)
+        val forecastTableThree = ForecastFactory.createForecast().copy(weatherId = 3)
+
+        database.weatherDao().insert(weatherTableOne, weatherTableTwo, weatherTableThree).test()
+        database.forecastDao().insert(forecastTableOne, forecastTableTwo, forecastTableThree).test()
+
+        // Act
+        val testObserver = database.forecastDao().getForecastById(2).test()
+
+        // Assert
+        testObserver
+            .assertComplete()
+            .assertValue(forecastTableTwo.copy(id = 2L))
+    }
+
+    @Test
+    fun `should count the correct number of records in the forecast table`() {
+        // Arrange
+        val weatherTableList = WeatherFactory.createWeatherList(10)
+        val forecastTableList = ForecastFactory.createForecastList(10)
+
+        database.weatherDao().insert(weatherTableList).test()
+        database.forecastDao().insert(forecastTableList).test()
+
+        // Act
+        val testObserver = database.forecastDao().count().test()
+
+        // Assert
+        testObserver
+            .assertComplete()
+            .assertValue(10)
+    }
+
+    @Test
+    fun `should delete a record from the forecast table`() {
+        // Arrange
+        val weatherTableOne = WeatherFactory.createWeather()
+        val weatherTableTwo = WeatherFactory.createWeather()
+        val weatherTableThree = WeatherFactory.createWeather()
+
+        val forecastTableOne = ForecastFactory.createForecast().copy(weatherId = 1)
+        val forecastTableTwo = ForecastFactory.createForecast().copy(weatherId = 2)
+        val forecastTableThree = ForecastFactory.createForecast().copy(weatherId = 3)
+
+        database.weatherDao().insert(weatherTableOne, weatherTableTwo, weatherTableThree).test()
+        database.forecastDao().insert(forecastTableOne, forecastTableTwo, forecastTableThree).test()
+
+        // Act
+        val testObserver = database.forecastDao().delete(forecastTableTwo.copy(id = 2)).test()
+
+        // Assert
+        testObserver
+            .assertComplete()
+            .assertValue(1)
+    }
+
+    @Test
+    fun `should update a record in the forecast table`() {
+        // Arrange
+        val weatherTableOne = WeatherFactory.createWeather()
+        val weatherTableTwo = WeatherFactory.createWeather()
+        val weatherTableThree = WeatherFactory.createWeather()
+
+        val forecastTableOne = ForecastFactory.createForecast().copy(weatherId = 1)
+        val forecastTableTwo = ForecastFactory.createForecast().copy(weatherId = 2)
+        val forecastTableThree = ForecastFactory.createForecast().copy(weatherId = 3)
+
+        database.weatherDao().insert(weatherTableOne, weatherTableTwo, weatherTableThree).test()
+        database.forecastDao().insert(forecastTableOne, forecastTableTwo, forecastTableThree).test()
+
+        // Act
+        val testObserver = database.forecastDao().update(forecastTableTwo.copy(id = 2, highTemp = 45.7F)).test()
+
+        // Assert
+        testObserver
+            .assertComplete()
+            .assertValue(1)
+    }
 }
